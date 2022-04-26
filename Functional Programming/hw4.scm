bbeae9462f7f5d50cd7d5ed8aef7a94bd9ad4f91
Aidan Rutherford <62723997+aidanTheCoder@users.noreply.github.com>
2022-04-26 09:42:08 -0700

    Update hw4.scm

--- Functional Programming/hw4.scm
+++ Functional Programming/hw4.scm
@@ -1,13 +1,11 @@
 (define mycount
     (lambda (x L)
-    (define i 0)
     (cond ((null? L) 0)
-          ((= x (car L)) (+ i 1 (mycount x (cdr L))))
-          (else (+ i (mycount x (cdr L)))))))
+          ((= x (car L)) (+ 1 (mycount x (cdr L))))
+          (else (mycount x (cdr L))))))
 
 (define tagmatch 
   (lambda (x L)
-  (define T '())
     (cond ((null? L) '())
           ((= x (car L)) (cons 1 (tagmatch x (cdr L))))
           (else (cons 0 (tagmatch x (cdr L)))))))
